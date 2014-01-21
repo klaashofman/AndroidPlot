4bd4da3b638171cb812c2efa71c767f4e4d1af4a
Klaas Hofman <Klaas.hofman@essensium.com>
2014-01-21 18:20:23 +0100

    todo: fix the missing path

--- src/com/example/androidplot/MainActivity.java
+++ src/com/example/androidplot/MainActivity.java
@@ -1,17 +1,33 @@
 package com.example.androidplot;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 
-public class MainActivity extends Activity {
 
+
+public class MainActivity extends Activity {
+		
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
 	}
 
 	@Override
@@ -23,8 +39,12 @@ public class MainActivity extends Activity {
 	
 	public void onButton1Clicked(View view)
 	{
+		// Show the XY plot
 		Intent enabler = new Intent(this, SimpleXYPlotActivity.class);
 		startActivity(enabler);
+		
 	}
+	
 
+	
 }
--- src/com/example/androidplot/SimpleXYPlotActivity.java
+++ src/com/example/androidplot/SimpleXYPlotActivity.java
@@ -1,10 +1,17 @@
 package com.example.androidplot;
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.WindowManager;
-import com.androidplot.xy.SimpleXYSeries;
-import com.androidplot.xy.XYSeries;
 import com.androidplot.xy.*;
+//import com.example.androidplot.MainActivity.Data;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Arrays;
  
 /**
@@ -14,11 +21,111 @@ public class SimpleXYPlotActivity extends Activity
 {
  
     private XYPlot plot;
- 
+    public class Data{
+		Number date[];
+		Number temp[];
+		Number humid[];
+	}
+	
+	public static final String SENSOR_DATA_FILENAME = "sensors.csv";
+	public static final String SENSOR_DATA_DIRNAME="AndroidPlot";
+	
+	private Data mySensorData = new Data();
+	public void getSensorData(Data data){
+		
+		if ( !isExternalStorageReadable() ) {
+			Log.d("androidplot", "no external storage present, aborting");
+			finish();
+		}
+		if ( !readSensorDataFile(data) ) {
+			Log.d("androidplot", "no storage data file present, aborting");
+			finish();
+		}
+			
+	}
+	
+	/* Checks if external storage is available to at least read */
+	public boolean isExternalStorageReadable() {
+	    String state = Environment.getExternalStorageState();
+	    if (Environment.MEDIA_MOUNTED.equals(state) ||
+	        Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+	        return true;
+	    }
+	    return false;
+	}
+	
+	public static boolean createDirIfNotExists(String path) {
+	    boolean ret = true;
+
+	    File file = new File(Environment.getExternalStorageDirectory(), path);
+	    if (!file.exists()) {
+	        if (!file.mkdirs()) {
+	            Log.e("adroidplot :: ", "Problem creating sensor data folder");
+	            ret = false;
+	        }
+	    }
+	    return ret;
+	}
+	
+	public File getExternalStoragePrivateFile() throws IOException{
+		
+		createDirIfNotExists(SENSOR_DATA_DIRNAME);	
+	    File file = new File(getExternalFilesDir(SENSOR_DATA_DIRNAME), SENSOR_DATA_FILENAME);
+	    Log.d("androidplot", "file:" + file.getAbsolutePath());
+	    return file;
+	}
+	
+	
+	public void getCSVData(Data data, File file){
+		
+		BufferedReader reader = null;
+		try {
+			reader = new BufferedReader(new FileReader(file.getPath()));
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		try{
+			String line;
+			int i = 0;
+			while ((line = reader.readLine()) != null){
+				String[] RowData = line.split(",");
+				data.date[i] = Integer.parseInt(RowData[0]);
+				data.temp[i] = Float.parseFloat(RowData[1]);
+				data.humid[i] = Float.parseFloat(RowData[2]);
+				i++;
+			}
+		} catch (Exception e) {
+			 Log.e("adroidplot :: ", "error reading csv file", e);
+		}
+		finally {
+			try {
+	            reader.close();
+	        }
+	        catch (IOException e) {
+	            // handle exception
+	        	Log.e("adroidplot :: ", "error closing csv file", e);
+	        }
+		}	
+			
+	}
+	public boolean readSensorDataFile(Data data){
+		try {
+			File file = getExternalStoragePrivateFile();
+			getCSVData(data, file);
+			
+		} catch (IOException e) {
+	    	Log.w("androidplot", "error reading sensor data file ", e);
+	    	return false;
+	    }
+		return true;
+	}
+	
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
-        super.onCreate(savedInstanceState);
+    	getSensorData(mySensorData);
+    	super.onCreate(savedInstanceState);
  
         // fun little snippet that prevents users from taking screenshots
         // on ICS+ devices :-)
@@ -30,18 +137,19 @@ public class SimpleXYPlotActivity extends Activity
         // initialize our XYPlot reference:
         plot = (XYPlot) findViewById(R.id.mySimpleXYPlot);
  
-        // Create a couple arrays of y-values to plot:
-        Number[] series1Numbers = {1, 8, 5, 2, 7, 4};
-        Number[] series2Numbers = {4, 6, 3, 8, 2, 10};
- 
+        // Create a couple arrays of y-values to plot:      
+       
+        Number[] numberSeriesTemperature = mySensorData.temp;
+        Number[] numberSeriesHumidity = mySensorData.humid;
+        
         // Turn the above arrays into XYSeries':
         XYSeries series1 = new SimpleXYSeries(
-                Arrays.asList(series1Numbers),          // SimpleXYSeries takes a List so turn our array into a List
+                Arrays.asList(numberSeriesTemperature),          // SimpleXYSeries takes a List so turn our array into a List
                 SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, // Y_VALS_ONLY means use the element index as the x value
-                "Series1");                             // Set the display title of the series
+                "Temperature");                             // Set the display title of the series
  
         // same as above
-        XYSeries series2 = new SimpleXYSeries(Arrays.asList(series2Numbers), SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, "Series2");
+        XYSeries series2 = new SimpleXYSeries(Arrays.asList(numberSeriesHumidity), SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, "Humidity");
  
         // Create a formatter to use for drawing a series using LineAndPointRenderer
         // and configure it from xml:
